9645a2afae16b524a06662f9d09628b27e524937
tusharpandey <tushar.2april@gmail.com>
2014-12-09 15:42:28 +0530

    Ravi
    for loop added in the class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
 	public static void main(String args[]) {
+
 		System.out.println("hello");
+		
+		for ( int i = 0 ; i < 4 ; i ++ )
+		{
+			System.out.println("ith value : "+i);
+		}
 	}
 }
