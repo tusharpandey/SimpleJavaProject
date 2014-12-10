3e2b99ddf3c3a0b33302d6598bc9abbd11374f6b
tusharpandey <tushar.2april@gmail.com>
2014-12-10 11:15:51 +0530

    latlong added
    latlong class added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("hello");
 		System.out.println("hello");
+		
+		System.out.println(System.getProperty("sun.arch.data.model")) ;
 	}
 }
